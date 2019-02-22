88407751349deed3a6bab57c6a9dd6540ee5018b
Max Lettenberger <maxlettenberger@gmail.com>
2019-02-21 23:09:13 -0500

    Working app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question/question.component';
+
+const routes: Routes = [{ path: 'question', component: QuestionComponent }];;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/question/question.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.css']
+})
+export class QuestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  questions: Array<string> = ["Last year over 70,000 women took to the streets of Seoul to protest the wave of revenge porn", "The founder of the Me Too Movement is Tarana Burke", "One in six American women will be raped at some point in their lives", "Eight percent of rapes occur while the victim is at work", "13% of women who are raped attempt suicide", "More than 90% of sexual assault victims on college campuses do not report the assault", "It is estimated that 60 percent of chronically hungry people in the world are women and girls", "Women make up more than two-thirds of the world's 796 million illiterate people", "The United States has never had a female-identifying president"];
+
+  answer: string = "True";
+
+  counter: number = 0;
+
+  handleAnswer() {
+    alert("Correct answer: True");
+    this.counter++;
+    if (this.counter==9) {
+      alert("Thanks for learning")
+    }
+  }
+}
